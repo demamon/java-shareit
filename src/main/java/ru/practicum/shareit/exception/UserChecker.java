8c7ff01dcbd8b64a3179d010fc14fac4157f6992
demamon <d-ba@yandex.ru>
2024-12-25 23:18:05 +0500

    Выполнил 14 тз

--- src/main/java/ru/practicum/shareit/exception/UserChecker.java
+++ src/main/java/ru/practicum/shareit/exception/UserChecker.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class UserChecker {
-    public static void emailChecker(String email){
-        if (email == null || email.isBlank()){
+    public static void emailChecker(String email) {
+        if (email == null || email.isBlank()) {
             log.error("Почта не может быть пустой");
             throw new ValidationException("почта не может быть пустой");
         }

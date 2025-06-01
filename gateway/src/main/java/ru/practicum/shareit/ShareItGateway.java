555762502bf9e548a2810850451e2877d3443bf6
demamon <d-ba@yandex.ru>
2025-06-01 20:12:53 +0500

    Выполнил 16 ТЗ

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Server {
-
+public class ShareItGateway {
 	public static void main(String[] args) {
-		SpringApplication.run(Server.class, args);
+		SpringApplication.run(ShareItGateway.class, args);
 	}
 
 }
